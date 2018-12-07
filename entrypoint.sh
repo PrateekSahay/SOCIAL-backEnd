191eee4dc3284b43647e757a880732a6bc0651b0
Nishant Jain <nishantkumarajain@gmail.com>
2018-12-07 10:35:34 +0530

    Removed ./ typo from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ until dotnet ef database update; do
 sleep 1
 done
 
-./ wait-for-it.sh -t 0 neo4j:7474 -- echo "neo4j is up"
+wait-for-it.sh -t 0 neo4j:7474 -- echo "neo4j is up"
 
 >&2 echo "SQL Server is up - executing command"
 exec $run_cmd
