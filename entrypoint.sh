27a87c69a3726c249d3a527938cc3c04a1b043dd
Nishant Jain <nishantkumarajain@gmail.com>
2018-12-07 13:27:09 +0530

    Updated the entrypoint.sh to do Migrations before updating the database

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 set -e
 run_cmd="dotnet run --server.urls http://*:81"
 
-until dotnet ef database update; do
+until dotnet ef migrations add InitialMigrations; do
 >&2 echo "SQL Server is starting up"
 sleep 1
 done
+
+dotnet ef database update
 
 ./wait-for-it.sh -t 0 neo4j:7474 -- echo "neo4j is up"
 
